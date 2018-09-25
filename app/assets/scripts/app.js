68161a048ec70654470b338027dfe72a78a7976a
deeadebayo <deeadebayo@gmail.com>
2018-09-24 20:58:38 -0400

    webpack/babel/es6 all good to go

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,11 +1,20 @@
-const Person = require('./modules/Person'),
-    $ = require('jquery');
+const $ = require('jquery');
 
+// const Person = require('./modules/Person');
+import Person from './modules/Person';
+
+
+class Adult extends Person {
+    payTaxes() {
+        console.log(`${this.name} owes $0 in taxes`);
+    }
+}
 
 alert(`abc 321`);
 
 let john = new Person('John Doe', 'green');
 john.greet();
 
-let jane = new Person('Jane Smith', 'blue');
+let jane = new Adult('Jane Smith', 'blue');
 jane.greet();
+jane.payTaxes();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,9 +1,12 @@
-function Person(fullName, favColor) {
-    this.name = fullName;
-    this.color = favColor;
-    this.greet = () => {
-        console.log(`Hello, my name is ${this.name} and my favorite color is ${this.color}.`);
+class Person {
+    constructor(fullName, favColor) {
+        this.name = fullName;
+        this.color = favColor;
+    }
+
+   greet() {
+        console.log(`Hello there, my name is ${this.name} and my favorite color is ${this.color}.`);
     }
 }
 
-module.exports = Person;
+export default Person;
